0ac3886de40076d2788d8e55a3e7bdfeabbf048c
Bermilion <bermilion@studiobrain.ru>
2026-05-08 12:25:49 +0300

    Правки компонента иконки

--- src/View/Components/Icon.php
+++ src/View/Components/Icon.php
@@ -1,5 +1,7 @@
 <?php namespace Chunker2i\Base\View\Components;
 
+use Illuminate\Support\Facades\File;
+
 class Icon extends AbstractComponent
 {
     public string $name;
@@ -20,7 +22,65 @@ class Icon extends AbstractComponent
 
     public function render()
     {
-        return view('chunker::components.icon');
+        $svgData = $this->svgData();
+
+        return view('chunker::components.icon', [
+            'isColored' => $this->isColored(),
+            'svgAttrs' => $svgData['attrs'] ?? null,
+            'svgContent' => $svgData['content'] ?? null,
+        ]);
+    }
+
+    /**
+     * Проверяет, является ли иконка цветной (лежит в icons-colored)
+     */
+    public function isColored(): bool
+    {
+        $coloredPath = resource_path("icons-colored/{$this->name}.svg");
+        return File::exists($coloredPath);
+    }
+
+    /**
+     * Получает структурированные данные SVG для цветной иконки
+     * @return array|null ['attrs' => [...], 'content' => '...']
+     */
+    protected function svgData(): ?array
+    {
+        if (!$this->isColored()) {
+            return null;
+        }
+
+        $path = resource_path("icons-colored/{$this->name}.svg");
+        $content = File::get($path);
+
+        // Парсим SVG
+        $dom = new \DOMDocument();
+        @$dom->loadXML($content, LIBXML_NOERROR | LIBXML_NOWARNING);
+
+        $svgElement = $dom->getElementsByTagName('svg')->item(0);
+        if (!$svgElement) {
+            return null;
+        }
+
+        // Собираем атрибуты
+        $attrs = [];
+        foreach ($svgElement->attributes as $attr) {
+            $attrs[$attr->name] = $attr->value;
+        }
+
+        // Удаляем width/height (управляем через CSS)
+        unset($attrs['width'], $attrs['height']);
+
+        // Получаем внутреннее содержимое
+        $innerContent = '';
+        foreach ($svgElement->childNodes as $child) {
+            $innerContent .= $dom->saveXML($child);
+        }
+
+        return [
+            'attrs' => $attrs,
+            'content' => trim($innerContent),
+        ];
     }
 
     public function classes(): string
